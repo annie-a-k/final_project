e5d01f306783136889c33deb6ca61b603b6fa59f
annie-a-k <anna.kalashnikova2017@gmail.com>
2021-06-10 14:39:36 +0300

    Add

--- demo_app.py
+++ demo_app.py
@@ -106,16 +106,16 @@ with st.echo(code_location='below'):
                 divide_el = element.index("-")
                 if not divide_el==0 and not divide_el==len(element)-1:
                     a = re.search(r'\d*', element[0: divide_el])[0]
-                    if not a="":
+                    if not a=="":
                         start_el = int(a)
                     a = re.search(r'\d*', element[divide_el + 1:])[0]
-                    if not a="":
+                    if not a=="":
                         end_el = int(a)
                     for i in range(end_el - start_el + 1):
                         find_code.append(start_el + i)
             else:
                 a = re.search(r'\d*', element)[0]
-                if not a="":
+                if not a=="":
                     find_code.append(int(a))
         st.write(find_code)
         for element in find_code:
